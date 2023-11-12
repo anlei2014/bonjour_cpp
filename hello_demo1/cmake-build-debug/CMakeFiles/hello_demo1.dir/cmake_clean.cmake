f3199cadd168091bc48e638a3474567a8bbbd5d8
anlei2014 <404280116@qq.com>
2023-11-12 15:23:52 +0800

    macos

---
+++ hello_demo1/cmake-build-debug/CMakeFiles/hello_demo1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_demo1.dir/main.cpp.o"
+  "CMakeFiles/hello_demo1.dir/main.cpp.o.d"
+  "hello_demo1"
+  "hello_demo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
